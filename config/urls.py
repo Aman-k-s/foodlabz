55e4de2efe60e5e389b950678b002767fe33e138
Aman-k-s <aks211531@gmail.com>
2026-04-04 15:42:54 +0530

    coolify changes

--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,24 @@ URL configuration for config project.
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
+
+def home(request):
+    return HttpResponse(
+        "Foodlabz API is running.",
+        content_type="text/plain; charset=utf-8",
+    )
+
+
+def favicon(request):
+    return HttpResponse(status=204)
+
+
 urlpatterns = [
+    path("", home),
+    path("favicon.ico", favicon),
     path("admin/", admin.site.urls),
     path("api/", include("verification.urls")),
 ]
